9e5b8549535ce1e13dbea4194a27de73e70aaac2
Tom Hodgkinson <hdgknsn@gmail.com>
2020-10-09 21:46:13 -0400

    refactored code

--- src/treasure_map/TreasureMap.java
+++ src/treasure_map/TreasureMap.java
@@ -12,55 +12,46 @@ public class TreasureMap {
 			String location = Integer.toString(bombLoc[0]) + Integer.toString(bombLoc[1]);
 			bombMap.put(location, 0);
 		}
-		
+
 		int[] result = new int[2];
-		for(int i = 0; i < directions.length; i++) {
+		for (int[] direction : directions) {
 			int xFrom = x;
 			int yFrom = y;
-			switch(directions[i][0]) {
+			switch(direction[0]) {
 				case 0:
-					y -= directions[i][1];
+					y -= direction[1];
 					break;
 				case 1:
-					x += directions[i][1];
+					x += direction[1];
 					break;
 				case 2:
-					y += directions[i][1];
+					y += direction[1];
 					break;
 				case 3:
-					x -= directions[i][1];
+					x -= direction[1];
 					break;
 			}
+			result[0] = x;
+			result[1] = y;
+			
+//			interimResult =  bombCheck(bombMap, xFrom, yFrom, x, y) == true ? new int[] {-1} : result;
 			
 			if(bombCheck(bombMap, xFrom, yFrom, x, y)) {
 				return new int[] {-1};
 			}
 			
-			result[0] = x;
-			result[1] = y;
+
 		}
 		return result;
 	}
 	
 	private static boolean bombCheck(Map<String, Integer> bombMap, int xFrom, int yFrom, int x, int y) {
-		if(yFrom == y) {
-			int xfrom = Integer.valueOf(Integer.toString(xFrom) + Integer.toString(yFrom));
-			int xto = Integer.valueOf(Integer.toString(x) + Integer.toString(y));
-			for(int i = Math.min(xfrom, xto); i <= Math.max(xfrom , xto); i += 10) {
-				String test = Integer.toString(i);
-				if(bombMap.containsKey(test)) {
-					return true;
-				}
-			}
-
-		} else if (xFrom == x) {
-			int yfrom = Integer.valueOf(Integer.toString(xFrom) + Integer.toString(yFrom));
-			int yto = Integer.valueOf(Integer.toString(x) + Integer.toString(y));
-			for(int i = Math.min(yfrom, yto); i <= Math.max(yfrom, yto); i++) {
-				String test = Integer.toString(i);
-				if(bombMap.containsKey(test)) {
-					return true;
-				}
+		int from = Integer.valueOf(Integer.toString(xFrom) + Integer.toString(yFrom));
+		int to = Integer.valueOf(Integer.toString(x) + Integer.toString(y));
+		int skipValue = yFrom == y ? 10 : 1;
+		for(int i = Math.min(from, to); i <= Math.max(from , to); i += skipValue) {
+			if(bombMap.containsKey(Integer.toString(i))) {
+				return true;
 			}
 		}
 		return false;
--- src/treasure_map/mAIN.java
+++ src/treasure_map/mAIN.java
@@ -14,7 +14,7 @@ public class Main {
 		};
 		
 		int[][] bombLocs = new int[][] {
-			{2, 4}
+			{2, 3}
 		};
 		System.out.println(Arrays.toString(TreasureMap.treasureFinder(2, 2, directions, bombLocs)));
 	}
